a528df405c8d52edb69c05f77543b8a51fe36cb3
Eric Takahashi <erictakahashi@gmail.com>
2025-01-26 14:33:25 -0300

    feat(tailwind): add tailwind, set it up, and create a home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Home } from './modules/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      React Post Mosaic
-    </>
-  )
+  return <Home />
 }
 
 export default App
---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,10 @@
+/**
+ * The home page component.
+ */
+export const Home = () => {
+  return (
+    <div className="grid h-screen w-screen place-items-center">
+      <div className="w-full h-full max-w-7xl">Home</div>
+    </div>
+  )
+}
